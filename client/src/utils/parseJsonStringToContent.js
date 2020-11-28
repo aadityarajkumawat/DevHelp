aad07e9547000a61e3744f1cd09b6430debfa07f
Aditya Rak Kumawat <arkumawat78@gmail.com>
2020-11-28 23:26:47 +0530

    trying to figure out parser

--- client/src/utils/parseJsonStringToContent.js
+++ client/src/utils/parseJsonStringToContent.js
@@ -1,11 +1,96 @@
+import React from "react";
+
 export const parseJsonStringToContent = (contentString) => {
   const contentArray = JSON.parse(contentString);
+  let finalContent = [];
 
-  let finalContentString = "";
+  for (let i = 0; i < contentArray.length; i++) {
+    let { text, inlineStyleRanges, key } = contentArray[i];
+    if (text !== "" || text !== " ") {
+      let finalLine = [];
+      if (inlineStyleRanges.length > 0) {
+        for (let j = 0; j < inlineStyleRanges.length; j++) {
+          if (
+            inlineStyleRanges[i] !== undefined &&
+            inlineStyleRanges[i].style === "BOLD"
+          ) {
+            if (i === 0) {
+              finalLine.push(
+                <span>{text.substring(0, inlineStyleRanges[i].offset)}</span>
+              );
+              finalLine.push(
+                <strong>
+                  {text.substring(
+                    inlineStyleRanges[i].offset,
+                    inlineStyleRanges[i].offset + inlineStyleRanges[i].length
+                  )}
+                </strong>
+              );
+            } else {
+              finalLine.push(
+                <span>
+                  {text.substring(
+                    inlineStyleRanges[i - 1].offset +
+                      inlineStyleRanges[i - 1].length,
+                    inlineStyleRanges[i].offset
+                  )}
+                </span>
+              );
+              finalLine.push(
+                <strong>
+                  {text.substring(
+                    inlineStyleRanges[i].offset,
+                    inlineStyleRanges[i].offset + inlineStyleRanges[i].length
+                  )}
+                </strong>
+              );
+            }
+          } else if (
+            inlineStyleRanges[i] !== undefined &&
+            inlineStyleRanges[i].style === "ITALIC"
+          ) {
+            if (i === 0) {
+              finalLine.push(
+                <span>{text.substring(0, inlineStyleRanges[i].offset)}</span>
+              );
+              finalLine.push(
+                <i>
+                  {text.substring(
+                    inlineStyleRanges[i].offset,
+                    inlineStyleRanges[i].offset + inlineStyleRanges[i].length
+                  )}
+                </i>
+              );
+            } else {
+              finalLine.push(
+                <span>
+                  {text.substring(
+                    inlineStyleRanges[i - 1].offset +
+                      inlineStyleRanges[i - 1].length,
+                    inlineStyleRanges[i].offset
+                  )}
+                </span>
+              );
+              finalLine.push(
+                <i>
+                  {text.substring(
+                    inlineStyleRanges[i].offset,
+                    inlineStyleRanges[i].offset + inlineStyleRanges[i].length
+                  )}
+                </i>
+              );
+            }
+          }
+        }
+      } else {
+        finalContent.push(<span>{text}</span>);
+      }
 
-  contentArray.map((line) => {
-    finalContentString += line.text;
-  });
+      finalContent.push(<span key={key}>{finalLine}</span>);
+    } else {
+      finalContent.push(<br />);
+    }
+  }
 
-  return finalContentString;
+  return finalContent;
 };
